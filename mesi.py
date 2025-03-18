f1007621d1f34b5eb9e79e351061a9a6a728a660
federico_degrandis <dgfederico44@gmail.com>
2025-03-18 16:19:01 +0100

    creato giorni.py

---
+++ mesi.py
@@ -0,0 +1,10 @@
+GIORNI = ("lunedì    " +
+    "martedì   " +       
+    "mercoledì      " +
+    "giovedì     " +
+    "venerdì     " +
+    "sabato     " +
+    "domenica     ")
+
+giorno = int(input("Indica giorno: "))
+print(GIORNI(mese - 1) * 9 : (mese - 1 -9))
