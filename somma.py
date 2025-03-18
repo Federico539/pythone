cc506f4c6923cb4d75974fc64fd928d5dd8f918d
federico_degrandis <dgfederico44@gmail.com>
2025-03-18 12:58:46 +0100

    creazione file somma.py

---
+++ somma.py
@@ -0,0 +1,8 @@
+valoreX = input("passami il primo valore:")
+valoreY = input("passimi il secondo valore:")
+v1 = int(valoreX)
+v2 = int(valoreY)
+
+somma = v1 + v2
+
+print("il risultato della somma è", somma)
